72cea2c720a077702b9c466086c447a5b14b0123
SeifHassan <seif.hassan@trianglz.com>
2023-03-24 16:39:54 +0200

    nestjs-mongodb-crud

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { StudentSchema } from './schema/student.schema';
+import { StudentService } from './student/student.service';
+import { StudentController } from './student/student.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017', {
+      dbName: 'studentdb',
+    }),
+    MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
+  ],
+  controllers: [AppController, StudentController],
+  providers: [AppService, StudentService],
+})
+export class AppModule {}
---
+++ src/dto/update-student.dto.ts
@@ -0,0 +1,3 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateStudentDto } from './create-student.dto';
+export class UpdateStudentDto extends PartialType(CreateStudentDto) {}
